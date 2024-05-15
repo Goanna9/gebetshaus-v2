5ea341faf7af0528ad11f27282f2f8667817198b
Robin Stauferth <robinstauferth92@yahoo.de>
2024-05-15 18:58:48 +0200

    added noscroll when mobile menu is opened

--- scripts/hamburger.js
+++ scripts/hamburger.js
@@ -1,7 +1,9 @@
 const hamMenu = document.querySelector('.hamburger');
 const offScreenMenu = document.querySelector('.offscreen-menu');
+const body = document.querySelector('body');
 
 hamMenu.addEventListener('click', () => {
+    body.classList.toggle('no-scroll');
     offScreenMenu.classList.toggle('active');
     hamMenu.classList.toggle('opened');
     hamMenu.setAttribute('aria-expanded');
